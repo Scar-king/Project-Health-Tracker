ec929f87c278104c2a4cf9ef558ba6ba080376a4
DoDavin <dodavin96@gmail,com>
2025-03-05 13:22:21 +0700

    Update

--- Project/Main.java
+++ Project/Main.java
@@ -2,15 +2,12 @@ package Project;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
 
         InputData input = new InputData();
-=======
-        InputBMIData input = new InputBMIData();
->>>>>>> b95019a5ba2a6adbc48a8d22feed9bfa479b3ef6
         Menu menu = new Menu();
 
         menu.welcome();
         input.InputValue();
+
     }
 }
--- Project/Menu.java
+++ Project/Menu.java
@@ -2,7 +2,6 @@ package Project;
 
 public class Menu {
 
-<<<<<<< HEAD
     void welcome(){
         System.out.println(Colors.BRIGHT_YELLOW + 
     "\t█████████████████████████████████████████████\n" +
@@ -30,26 +29,7 @@ public class Menu {
         "\t\t╚════════════════════════╝\n" + 
         Colors.RESET);
     
-=======
-    public void welcome() {
-        System.out.println("***********************************************");
-        System.out.println("**                                           **");
-        System.out.println("**      Welcome to the Health Tracker!       **");
-        System.out.println("**   Your journey to a healthier you starts  **");
-        System.out.println("**              right here!                  **");
-        System.out.println("**                                           **");
-        System.out.println("***********************************************");
-    }
-
-    public void displayMenu() {
-        System.out.println("------------------------");
-        System.out.printf("| %-10s       |\n", "Health Tracker");
-        System.out.println("------------------------");
-        System.out.printf("| %-14s       |\n", "1. BMI");
-        System.out.printf("| %-6s      |\n", "2. Step Tracker");
-        System.out.printf("| %-6s      |\n", "3. Exit");
-        System.out.println("------------------------");
-        System.out.print("Enter your choice: ");
->>>>>>> b95019a5ba2a6adbc48a8d22feed9bfa479b3ef6
     }
 }
+
+
